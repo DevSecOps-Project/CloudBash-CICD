54bbc4186de923ca51673aa4aadf6c3aec435aa8
dave <raismandavid@gmail.com>
2024-05-28 08:32:54 +0300

    fix aws cmd

--- stages/build_docker.py
+++ stages/build_docker.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 import re
@@ -12,7 +13,8 @@ def get_latest_image_tag(repository_name, aws_region):
             '--output', 'text',
             '--query', '"sort_by(imageDetails,&imagePushedAt)[-1].imageTags[0]"'
         ]
-        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        cmd = ' '.join(command)
+        result = os.system(cmd)
         latest_tag = json.loads(result.stdout.strip().strip('"'))
         print(f'Latest image tag: {latest_tag}')
         return latest_tag
