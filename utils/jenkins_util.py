7f56bb04c7620409b44bcebbc4383d3cbecfc034
dave <raismandavid@gmail.com>
2024-06-05 22:26:32 +0300

    fix

--- utils/jenkins_util.py
+++ utils/jenkins_util.py
@@ -9,7 +9,7 @@ def get_job_data():
     job_data['build_url'] = os.getenv('BUILD_URL')
     job_data['build_status'] = os.getenv('BUILD_STATUS')
     job_data['build_url'] = "".join(
-        "http://localhost:{}}/job/{}/{}".format(
+        "http://localhost:{}/job/{}/{}".format(
             utils.constants.JENKINS.PORT,
             job_data['job_name'],
             job_data['build_number']
