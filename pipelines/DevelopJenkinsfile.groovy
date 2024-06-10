a49adc0a279785d8e0ff6d393b7a186185ca428d
Daver-Creations <76684653+Daver-Creations@users.noreply.github.com>
2024-06-10 20:04:12 +0300

    cicd_pipeline (#11)

---
+++ pipelines/DevelopJenkinsfile.groovy
@@ -0,0 +1,140 @@
+#!groovy
+
+import groovy.json.JsonSlurperClassic
+
+def scmData = null
+def logDir = "logs"
+
+pipeline {
+
+    parameters {
+        string(name: 'BRANCH', defaultValue: 'develop', description: 'The branch to build')
+    }
+
+    options {
+        timeout(time: 10, unit: 'MINUTES')
+        timestamps()
+        disableConcurrentBuilds()
+        buildDiscarder(logRotator(daysToKeepStr: '7', numToKeepStr: '10'))
+        skipDefaultCheckout true
+    }
+
+    agent any
+
+    environment {
+        VENV_DIR = "${WORKSPACE}/venv"
+        PYTHONPATH = "${WORKSPACE}/CloudBash-CICD"
+    }
+
+    stages {
+
+        stage('Checkout CICD') {
+            steps {
+                echo "${STAGE_NAME} Stage Execution Starting"
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: '*/${BRANCH}']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'CloudBash-CICD']],
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[
+                        credentialsId: 'da701aad-9139-4a8c-b2b9-32f5872d9de3',
+                        url: 'https://github.com/DevSecOps-Project/CloudBash-CICD.git'
+                    ]]
+                ])
+                dir('logs') {
+                    writeFile file: 'dummy.txt', text: ""
+                }
+            }
+            post {
+                success {
+                    echo "${STAGE_NAME} Stage Finished Successfully"
+                }
+                failure {
+                    echo "${STAGE_NAME} Stage Failed"
+                }
+            }
+        }
+
+        stage('Setup Environment') {
+            steps {
+                script {
+                    sh """#!/bin/bash
+                        python3 -m venv ${VENV_DIR}
+                        source ${VENV_DIR}/bin/activate
+                        pip install --upgrade pip
+                        pip install flask flask_restful requests pylint pytest
+                        python -c "import sys; print(sys.path)"
+                    """
+                }
+            }
+            post {
+                success {
+                    echo "${STAGE_NAME} Stage Finished Successfully"
+                }
+                failure {
+                    echo "${STAGE_NAME} Stage Failed"
+                }
+            }
+        }
+
+        // stage('Pylint') {
+        //     steps {
+        //         script {
+        //             sh """#!/bin/bash
+        //                 source ${VENV_DIR}/bin/activate
+        //                 pylint --disable=missing-docstring,W0201,W0718  ${WORKSPACE}/CloudBash-CICD
+        //             """
+        //         }
+        //     }
+        //     post {
+        //         success {
+        //             echo "${STAGE_NAME} Stage Finished Successfully"
+        //         }
+        //         failure {
+        //             echo "${STAGE_NAME} Stage Failed"
+        //         }
+        //     }
+        // }
+
+        stage('Unit Tests') {
+            steps {
+                script {
+                    sh """#!/bin/bash
+                        source ${VENV_DIR}/bin/activate
+                        export PYTHONPATH=${PYTHONPATH}
+                        pytest ${WORKSPACE}/CloudBash-CICD/tests
+                    """
+                }
+            }
+            post {
+                always {
+                    script {
+                        sh """#!/bin/bash
+                            deactivate || true
+                        """
+                    }
+                }
+                success {
+                    echo "${STAGE_NAME} Stage Finished Successfully"
+                }
+                failure {
+                    echo "${STAGE_NAME} Stage Failed"
+                }
+            }
+        }
+    }
+    
+    post {
+        always {
+            script {
+                if (currentBuild.result == null) {
+                    currentBuild.result = 'SUCCESS'
+                }
+                sh """#!/bin/bash
+                    BUILD_STATUS=${currentBuild.result} python3 CloudBash-CICD/notify.py
+                """
+            }
+        }
+    }
+}
---
+++ tests/test_executor.py
@@ -0,0 +1,54 @@
+import pytest
+import subprocess
+from unittest.mock import patch, Mock
+
+import utils.executor
+
+
+class TestExecutorUtil:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.command = []
+        self.result = None
+
+    def test_execute_command_success(self):
+        self.command = ["echo", "hello"]
+        mock_result = Mock()
+        mock_result.returncode = 0
+        mock_result.stdout = "hello\n"
+        mock_result.stderr = ""
+        with patch('subprocess.run', return_value=mock_result):
+            self.result = utils.executor.execute_command(self.command)
+            assert self.result == "hello\n"
+
+    def test_execute_command_failure(self):
+        self.command = ["false"]
+        with patch('subprocess.run',
+                   side_effect=subprocess.CalledProcessError(
+                       returncode=1,
+                       cmd=' '.join(self.command),
+                       output="",
+                       stderr="some error occurred"
+                    )
+                ):
+            self.result = utils.executor.execute_command(self.command)
+            assert self.result == {
+                'stdout': "",
+                'stderr': "some error occurred",
+                'returncode': 1,
+                'error': "Command 'false' returned non-zero exit status 1."
+            }
+
+    def test_execute_command_unexpected_exception(self):
+        self.command = ["some", "invalid", "command"]
+        with patch(
+            'subprocess.run',
+            side_effect=Exception("unexpected error")
+        ):
+            self.result = utils.executor.execute_command(self.command)
+            assert self.result == {
+                'stdout': None,
+                'stderr': None,
+                'returncode': None,
+                'error': "unexpected error"
+            }
