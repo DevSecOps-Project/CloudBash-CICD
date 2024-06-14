7cb638337228d561c330ae2e91942a032fa25770
dave <raismandavid@gmail.com>
2024-06-14 16:47:26 +0300

    sleep

--- upload_image.py
+++ upload_image.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 import utils.aws_util
 import utils.constants
@@ -10,13 +11,12 @@ if __name__ == "__main__":
     try:
         tagged_image = os.getenv('TAGGED_IMAGE')
         new_docker_tag = os.getenv('NEW_DOCKER_TAG')
-        print(f'NEW_DOCKER_TAG={new_docker_tag}')
-        print(f'TAGGED_IMAGE={tagged_image}')
         if not tagged_image or not new_docker_tag:
             raise ValueError('TAGGED_IMAGE or NEW_DOCKER_TAG environment variables not set')
         utils.aws_util.ecr_authenticate()
         utils.docker_util.push_docker_image_to_ecr(tagged_image)
         new_tag = utils.aws_util.strip_version_val(new_docker_tag)
+        time.sleep(10)
         if utils.aws_util.is_last_version(new_tag):
             print("Docker image uploaded successfully to ECR")
         else:
