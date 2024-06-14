3de493deed6ab236871eac3be770114adeb40705
dave <raismandavid@gmail.com>
2024-06-14 14:51:09 +0300

    better logic

--- upload_image.py
+++ upload_image.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import utils.aws_util
@@ -7,9 +8,10 @@ import utils.docker_util
 
 if __name__ == "__main__":
     try:
-        docker_tag = utils.aws_util.get_latest_image_version()
-        new_docker_tag = utils.docker_util.increment_tag(docker_tag)
-        tagged_image = utils.docker_util.tag_docker_image(new_docker_tag)
+        tagged_image = os.getenv('TAGGED_IMAGE')
+        new_docker_tag = os.getenv('NEW_DOCKER_TAG')
+        if not tagged_image or not new_docker_tag:
+            raise ValueError('TAGGED_IMAGE or NEW_DOCKER_TAG environment variables not set')
         utils.aws_util.ecr_authenticate()
         utils.docker_util.push_docker_image_to_ecr(tagged_image)
         new_tag = utils.aws_util.strip_version_val(new_docker_tag)
