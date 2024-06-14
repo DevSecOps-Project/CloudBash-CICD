d88419c59f31a19055618ee75ba6f843a3d448c0
dave <raismandavid@gmail.com>
2024-06-14 13:22:17 +0300

    validation and cmd fix

--- upload_image.py
+++ upload_image.py
@@ -12,7 +12,12 @@ if __name__ == "__main__":
         tagged_image = utils.docker_util.tag_docker_image(new_docker_tag)
         utils.aws_util.ecr_authenticate()
         utils.docker_util.push_docker_image_to_ecr(tagged_image)
-        print("Docker image uploaded successfully to ECR.")
+        new_tag = utils.aws_util.strip_version_val(new_docker_tag)
+        if utils.aws_util.is_last_version(new_tag):
+            print("Docker image uploaded successfully to ECR")
+        else:
+            print("Docker image upload to ECR failed")
+            raise ValueError(f'image {new_docker_tag} was not found on aws')
     except Exception as e:
         print(f"Error occurred while uploading Docker image to ECR: {e}")
         sys.exit(1)
--- utils/aws_util.py
+++ utils/aws_util.py
@@ -4,6 +4,9 @@ import utils.constants
 import utils.executor
 
 
+def strip_version_val(version):
+    return version.strip(" v'\n'\"")
+
 def get_latest_image_version():
     try:
         repository_name = utils.constants.AWS.ECR_REPO
@@ -12,10 +15,10 @@ def get_latest_image_version():
             '/opt/homebrew/bin/aws', 'ecr', 'describe-images',
             '--repository-name', repository_name,
             '--region', aws_region,
-            '--output', 'text',
-            '--query', '"sort_by(imageDetails,&imagePushedAt)[-1].imageTags[0]"'
+            '--output', 'json',
+            '--query', '"sort_by(imageDetails, &imagePushedAt)[-1].imageTags[0]"'
         ])
-        striped_tag = result.strip(" v'\n'")
+        striped_tag = strip_version_val(result)
         if striped_tag == '':
             raise ValueError("failed to get the latest image version")
         latest_tag = float(striped_tag)
@@ -40,3 +43,9 @@ def ecr_authenticate():
     except Exception as e:
         print(f'Error occurred while authenticating ECR: {e}')
         sys.exit(1)
+
+def is_last_version(version):
+    last_up_ver = get_latest_image_version()
+    if version == strip_version_val(last_up_ver):
+        return True
+    return False
