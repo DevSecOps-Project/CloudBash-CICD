2f3ced75e4e3bf0a25c2c006cf54cf3e1f3f44ff
dave <raismandavid@gmail.com>
2024-06-21 12:43:59 +0300

    update minikube util

--- utils/minikube_util.py
+++ utils/minikube_util.py
@@ -5,20 +5,11 @@ import utils.executor
 
 def minikube_is_active():
     try:
-        cmd = ['/opt/homebrew/bin/minikube', 'status']
+        cmd = ['/opt/homebrew/bin/minikube', 'status', "--format='{{.Host}}'"]
         output = utils.executor.execute_command(cmd)
-        active = (
-            'minikube\n'
-            'type: Control Plane\n'
-            'host: Running\n'
-            'kubelet: Running\n'
-            'apiserver: Running\n'
-            'kubeconfig: Configured\n\n'
-        )
         if 'returncode' in output:
-            if output['returncode'] == 7:
-                return False
-        elif output == active:
+            return False
+        elif output == "Running":
             return True
         raise Exception
     except Exception as e:
@@ -31,10 +22,14 @@ def start_minikube():
             print('Minikube is active')
             return
         else:
-            cmd = ['/opt/homebrew/bin/minikube', 'start', '--driver=/usr/local/bin/docker']
+            cmd = ['minikube', 'delete']
             output = utils.executor.execute_command(cmd)
-            print(output)
-            print('Minikube has been activated')
+            cmd = ['/opt/homebrew/bin/minikube', 'start', '--driver=docker']
+            output = utils.executor.execute_command(cmd)
+            if minikube_is_active():
+                print('Minikube has been activated')
+                return
+            raise Exception
     except Exception as e:
         print(f'Error occurred while starting Minikube: {e}')
         sys.exit(1)
@@ -58,7 +53,7 @@ def point_docker_daemon_to_minikube():
     try:
         cmd = ['eval', '$(/opt/homebrew/bin/minikube docker-env)']
         output = utils.executor.execute_command(cmd)
-        if output is '':
+        if output == '':
             print('Docker daemon points to Minikube')
             return
         raise Exception
