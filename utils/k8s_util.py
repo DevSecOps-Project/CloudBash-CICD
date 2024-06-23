3a29e29f39a95f04ba7eceeab3c45eda7f80155e
dave <raismandavid@gmail.com>
2024-06-23 17:31:36 +0300

    setup creds secret

--- utils/k8s_util.py
+++ utils/k8s_util.py
@@ -22,3 +22,39 @@ def k8s_apply(k8s_path, file):
     except Exception as e:
         print(f'Error occurred while applying the k8s file: {k8s_path}, {e}')
         sys.exit(1)
+
+def check_secrets(secret):
+    try:
+        cmd = ['kubectl', 'get', 'secret', '--all-namespaces']
+        output = utils.executor.execute_command(cmd)
+        if secret in output:
+            return True
+        return False
+    except Exception as e:
+        print(f'Error occurred while geting secrets: {e}')
+        sys.exit(1)
+
+def setup_creds_secret():
+    try:
+        creds_secret = 'creds-secret'
+        if check_secrets(creds_secret):
+            print('creds_secret exist')
+            return
+        aws_account_id = utils.constants.AWS.AWS_ACCOUNT_ID
+        aws_region = utils.constants.AWS.AWS_REGION
+        pass_stdin = f"{aws_account_id}.dkr.ecr.{aws_region}.amazonaws.com"
+        cmd = [
+            '/opt/homebrew/bin/kubectl',
+            'create secret', 'docker-registry',
+            creds_secret, f'--docker-server={pass_stdin}',
+            '--docker-username=AWS',
+            '--docker-password=$(aws ecr get-login-password)'
+        ]
+        utils.executor.execute_command(cmd)
+        if check_secrets(creds_secret):
+            print('creds_secret created successfully')
+            return
+        raise Exception
+    except Exception as e:
+        print(f'Error occurred while setting up creds secret: {e}')
+        sys.exit(1)
