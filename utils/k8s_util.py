8e4137f351fa92ff7d2194749000c7ce4904afd8
dave <raismandavid@gmail.com>
2024-06-23 22:02:36 +0300

    update creds secret

--- utils/k8s_util.py
+++ utils/k8s_util.py
@@ -24,29 +24,46 @@ def k8s_apply(k8s_path, file):
         print(f'Error occurred while applying the k8s file: {k8s_path}, {e}')
         sys.exit(1)
 
-def creds_secret_exist(secret):
+def age_to_seconds(age_str):
+    age_seconds = 0
+    time_units = {
+        's': 1,
+        'm': 60,
+        'h': 3600,
+        'd': 86400,
+        'w': 604800
+    }
+    pattern = re.compile(r'(\d+)([smhdw])')
+    for amount, unit in pattern.findall(age_str):
+        age_seconds += int(amount) * time_units[unit]
+    return age_seconds
+
+def valid_creds_secret(secret):
     try:
         cmd = ['./kubectl', 'get', 'secret', '--all-namespaces']
-        # cmd = ['kubectl', 'get', 'secret', '--all-namespaces']
         output = utils.executor.execute_command(cmd)
-        # re.search('default       creds-secret             kubernetes.io/dockerconfigjson   1      2d7h')
+        pattern = re.compile(r'default\s+creds-secret\s+\S+\s+\d+\s+(\S+)')
+        match = pattern.search(output)
+        age = match.group(1)
         if secret in output:
-            return True
+            one_day_seconds = 86400
+            age_in_seconds = age_to_seconds(age)
+            if age_in_seconds > one_day_seconds:
+                print(f"Age of creds-secret ({age}) is higher than 1 day")
+            else:
+                print(f"Age of creds-secret ({age}) is less than 1 day")
+                return True
         return False
     except Exception as e:
         print(f'Error occurred while geting secrets: {e}')
         sys.exit(1)
 
-def creds_secret_uptodate(creds_secret):
-    return False
-
 def setup_creds_secret():
     try:
         creds_secret = 'creds-secret'
-        if creds_secret_exist(creds_secret):
-            if creds_secret_uptodate(creds_secret):
-                print('creds_secret exist')
-                return
+        if valid_creds_secret(creds_secret):
+            print('creds_secret exist and is valid')
+            return
         cmd = ['./kubectl', 'delete', 'secret', creds_secret]
         utils.executor.execute_command(cmd)
         aws_account_id = utils.constants.AWS.AWS_ACCOUNT_ID
@@ -60,7 +77,7 @@ def setup_creds_secret():
             '--docker-password=$(/opt/homebrew/bin/aws ecr get-login-password)'
         ]
         utils.executor.execute_command(cmd)
-        if creds_secret_exist(creds_secret):
+        if valid_creds_secret(creds_secret):
             print('creds_secret created successfully')
             return
         raise Exception
